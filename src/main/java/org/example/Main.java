a857e051d75a22a64ccb5a1e8dc558313ce84e8d
krisrazumeiko <krisrazumeiko@gmail.com>
2025-05-19 19:58:05 +0300

    Задание к лекции 2.3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,47 @@
 package org.example;
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //1. Создать класс "Товар" с полями: название, дата производства, производитель, страна происхождения, цена, состояние бронирования покупателем.
+        //Конструктор класса должен заполнять эти поля при создании объекта.
+        //Внутри класса «Товар» написать метод, который выводит информацию об объекте в консоль.
+        Product prod1 = new Product("Phone", "11.11.2011","Apple", "USA", 10000, false);
+        Product prod2 = new Product("Phone", "23.11.2011","Samsung", "Korea", 9000, true);
+        Product prod3 = new Product("Laptop", "22.02.2019","Asus", "Taiwan", 12000, true);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        prod1.printInfo();
+        prod2.printInfo();
+        prod3.printInfo();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+        //  2. Создать массив из 5 товаров.
+        //  Пример:
+        //  вначале объявляем массив объектов
+        //  Product[] productsArray = new Product[5];
+        //  потом для каждой ячейки массива задаем объект
+        //  productsArray[0] = new Product("Samsung S25 Ultra", "01.02.2025", "Samsung Corp.", "Korea", 5599, true);
+        //  productsArray[1] = new Product(...);
+        //...
+        //  productsArray[4] = new Product(...);
+        Product[] prodArray = new Product[5];
+        prodArray[0] = new Product("Phone", "11.11.2011", "Apple", "USA", 10000, false);
+        prodArray[1] = new Product("Phone", "23.11.2011", "Samsung", "Korea", 9000, true);
+        prodArray[2] =new Product("Laptop", "22.02.2019", "Asus", "Taiwan", 12000, true);
+        prodArray[3] =new Product("TV", "22.12.2017", "Samsung", "Korea", 13000, false);
+        prodArray[4] =new Product("Laptop", "10.02.2020", "HP", "USA", 11000, true);
+
+        System.out.println(Arrays.toString(prodArray));
+
+
+        //3. Создать класс Park с внутренним классом, с помощью объектов которого можно хранить информацию об аттракционах, времени их работы и стоимости.
+        Park park = new Park();
+
+        Park.Attraction attraction = park.new Attraction("Колесо обозрения", "10.00 - 20.00", 10);
+        Park.Attraction attraction1 = park.new Attraction("Американские горки", "12.00 - 20.00", 15);
+        Park.Attraction attraction2 = park.new Attraction("Машинки", "10.00 - 21.00", 15);
+
+        attraction.printInfo();
+        attraction1.printInfo();
+        attraction2.printInfo();
     }
 }
