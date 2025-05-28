cd930d45739f457fc5b35a2af0ce771ce1c415a2
krisrazumeiko <krisrazumeiko@gmail.com>
2025-05-28 21:21:59 +0300

    Изменен файл MathMethodsTest

--- src/test/java/MathMethodsTest.java
+++ src/test/java/MathMethodsTest.java
@@ -2,18 +2,42 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.function.Executable;
+
 public class MathMethodsTest {
+    //Factorial
     @Test
-    void testFactorialNumber() {
+    void testFactorial_5() {
         assertEquals(120, MathMethods.factorialNumber(5));
+    }
+
+    @Test
+    void testFactorial_0() {
         assertEquals(1, MathMethods.factorialNumber(0));
     }
 
+    @Test
+    void testFactorial_1() {
+        assertEquals(1, MathMethods.factorialNumber(1));
+    }
+
+    //AreaTriangle
     @Test
     void testAreaTriangle() {
         assertEquals(6.0, MathMethods.areaTriangle(3, 4));
     }
 
+    @Test
+    void testAreaTriangle_a0() {
+        assertEquals(0.0, MathMethods.areaTriangle(0, 4));
+    }
+
+    @Test
+    void testAreaTriangle_h0() {
+        assertEquals(0.0, MathMethods.areaTriangle(3, 0));
+    }
+
+    //ArithmeticOperations
     @Test
     void testArithmeticOperations() {
         String expected = "Сумма чисел = 5\n" +
@@ -24,10 +48,38 @@ public class MathMethodsTest {
     }
 
     @Test
-    void testCompareNumbers() {
-        String expected = "a > b = true\n" +
-                "a < b = false\n" +
-                "a = b - false\n";
+    void testArithmeticOperations_DivByZero() {
+        Executable executable = new Executable() {
+            @Override
+            public void execute() {
+                MathMethods.arithmeticOperations(8, 0);
+            }
+        };
+        assertThrows(ArithmeticException.class, executable);
+    }
+
+    //CompareNumbers
+    @Test
+    void testCompareNumbers_() {
+        String expected = "a > b = true" + "\n" +
+                "a < b = false" + "\n" +
+                "a = b - false" + "\n";
         assertEquals(expected, MathMethods.compareNumbers(5, 3));
+    }
+
+    @Test
+    void testCompareNumbers_a_Less() {
+        String expected = "a > b = false" + "\n" +
+                "a < b = true" + "\n" +
+                "a = b - false" + "\n";
+        assertEquals(expected, MathMethods.compareNumbers(1, 3));
+    }
+
+    @Test
+    void testCompareNumbers_a_Equals_b() {
+        String expected = "a > b = false" + "\n" +
+                "a < b = false" + "\n" +
+                "a = b - true" + "\n";
+        assertEquals(expected, MathMethods.compareNumbers(5, 5));
     }
 }
