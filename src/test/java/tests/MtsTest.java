de731fa51d58ee45f8bce773987333802b8b6aaa
krisrazumeiko <krisrazumeiko@gmail.com>
2025-06-10 21:07:57 +0300

    Добавлено задание к лекции 2.10

---
+++ src/test/java/tests/MtsTest.java
@@ -0,0 +1,61 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pages.MtsOnlineTopUpBlockPage;
+
+import java.time.Duration;
+
+public class MtsTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    private MtsOnlineTopUpBlockPage paymentPage;
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("https://mts.by");
+        paymentPage = PageFactory.initElements(driver, MtsOnlineTopUpBlockPage.class);
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) driver.quit();
+    }
+
+    @Test
+    public void checkBlockTitle() {
+        paymentPage.checkBlockTitle();
+    }
+
+    @Test
+    public void checkPaymentSystemLogosPresent() {
+        paymentPage.checkPaymentSystemLogosPresent();
+    }
+
+    @Test
+    public void checkMoreAboutServiceLink() {
+        paymentPage.checkMoreAboutServiceLink();
+    }
+
+    @Test
+    public void checkServiceSelectionAndContinueButton() {
+        paymentPage.checkServiceSelectionAndContinueButton();
+    }
+
+    @Test
+    public void checkServiceSelectionForEachPaymentType() {
+        paymentPage.checkServiceSelectionForEachPaymentType();
+    }
+
+    @Test
+    public void checkPaymentFormFieldsAndIcons() {
+        paymentPage.checkPaymentFormFieldsAndIcons();
+    }
+}
